361a3749c60f481eb73941512d257961fbf16dcb
Saeed ul Haq <33142166+Saeed-ul-haq@users.noreply.github.com>
2020-06-08 09:52:53 +0500

    Updates

---
+++ precache-manifest.47df7c29d35f5e53322efde87b06198d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9777353438c02eb83c7f0c87efac1ce2",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "7c672b8f4c9970f07d45",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1a4f5698076803bebf5e",
+    "url": "/contactmanager/static/css/main.3fcd11d6.chunk.css"
+  },
+  {
+    "revision": "7c672b8f4c9970f07d45",
+    "url": "/contactmanager/static/js/2.b5ba0df8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.b5ba0df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4f5698076803bebf5e",
+    "url": "/contactmanager/static/js/main.ca383fa2.chunk.js"
+  },
+  {
+    "revision": "c5d6d7feaacb64bdb089",
+    "url": "/contactmanager/static/js/runtime-main.caa97656.js"
+  }
+]);
